14e3927630120a5b603921ae5f4eef98555b8ea9
Randee Johnson <randeejohnson@Randees-MacBook-Pro.local>
2021-05-16 21:39:22 -0400

    Building the Navigation Link Wrapper Styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -12,21 +12,36 @@ export default class NavigationComponent extends Component {
         return (
             <div className ="nav-wrapper">
                 <div className="left-side">
+
+                <div className="nav-link-wrapper">
                 <NavLink exact to="/">
                     Home
                 </NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
                 <NavLink to="/about-me" activeClassName="nav-link-active">
                     About
                 </NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
                 <NavLink to="/contact-me" activeClassName="nav-link-active">
                     Contact
                 </NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
                 <NavLink to="/blog" activeClassName="nav-link-active">
                     Blog
                 </NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
                 {false ? <NavLink to="/add-blog" activeClassName="nav-link-active">
                     Add Blog
                 </NavLink> : null}
+                </div>
                 </div>
 
                 <div className="right-side">
