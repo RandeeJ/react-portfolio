4ee7ffa1e54d96b89ee865ca7a13d4cd9b95359f
Randee Johnson <randeejohnson@Randees-MacBook-Pro.local>
2021-05-16 20:48:50 -0400

    Refactoring the PortfolioItem Component to Use Destructuring

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -3,12 +3,19 @@ import { Link } from "react-router-dom";
 
 
 export default function(props) {
+    //  Data that we'll need:
+            //  - Background Image : thumb_image_url
+            //  - Logo : logo_url
+            //  - Description : description
+            //  - ID (what we will use for the slug) : id
+            //  In console, call Object.keys(item) to get item keys
+            // ["id", "name", "description", "url", "category", "position", "thumb_image_url", "banner_image_url", "logo_url", "column_names_merged_with_images"]
+
+    const{ id, description, thumb_image_url, logo_url } = props.item
     return (
         <div>
-            <h3>{props.title}</h3>
-            <h4>{props.url}</h4>
-
-<Link to={`/portfolio/${props.slug}`}> Link </Link>
+    <div> {description} </div>
+    <Link to={`/portfolio/${id}`}> Link </Link>
             </div> 
     )
 }
