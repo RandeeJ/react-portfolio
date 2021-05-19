379da7ada56c3a12a7cb6bbdb987c74c29c8d461
Randee Johnson <randeejohnson@Randees-MacBook-Pro.local>
2021-05-18 22:24:27 -0400

    Updating Styles with React Event Listeners

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 
-export default function(props) {
+export default class PortfolioItem extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            portfolioItemClass: ""
+        };
+    }
     //  Data that we'll need:
             //  - Background Image : thumb_image_url
             //  - Logo : logo_url
@@ -10,13 +17,25 @@ export default function(props) {
             //  - ID (what we will use for the slug) : id
             //  In console, call Object.keys(item) to get item keys
             // ["id", "name", "description", "url", "category", "position", "thumb_image_url", "banner_image_url", "logo_url", "column_names_merged_with_images"]
+handleMouseEnter() {
+this.setState({portfolioItemClass: "image-blur"})
+}
 
-    const{ id, description, thumb_image_url, logo_url } = props.item
+handleMouseLeave() {
+this.setState({portfolioItemClass :""})
+}
+
+render() {
+    const{ id, description, thumb_image_url, logo_url } = this.props.item
     return (
-        <div className="portfolio-item-wrapper">
+        <div className="portfolio-item-wrapper"
+        onMouseEnter={() => this.handleMouseEnter()}
+        onMouseLeave={() => this.handleMouseLeave()}
+        >
+
 
 <div
-className="portfolio-img-background"
+className={"portfolio-img-background " + this.state.portfolioItemClass}
 style={{
     backgroundImage: "url(" + thumb_image_url + ")"
 }}
@@ -32,6 +51,6 @@ style={{
     </div>
         </div> 
     )
-}
+}}
 
 // backticks allow us to do more modern string interpolation
