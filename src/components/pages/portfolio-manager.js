e28adcdb6fa3b0d20d17c5666507fe28ea5a3428
Randee Johnson <randeejohnson@Randees-MacBook-Pro.local>
2021-06-04 22:22:13 -0400

    Portfolio Manager Grid Layout Implementation SOLO

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -3,13 +3,54 @@ import React, { Component } from 'react';
 export default class PortfolioManager extends Component {
     render() {
         return (
-            <div>
-                <h1>
-                    Portfolio Manager
-                </h1>
-                <h1>
-                    Portfolio Manager
-                </h1>
+            <div className="portfolio-manager-wrapper">
+                <div className="left-side">
+                <form>
+
+
+
+                    <div className="dataUpload">
+
+
+                        <div className="half-width row1">
+                    <input type="text" placeholder="Portfolio Item Name" />
+                    <input type="text" placeholder="URL"  />
+                    </div>
+
+                    <div className="half-width row2">
+                    <input type="text" placeholder="Position" />
+                    <input type="" />
+                    </div>
+
+
+
+                    <div className="full-width row3">
+                    <input type="text" placeholder="Description" />
+                    </div>
+
+                    </div>
+
+
+
+
+                    <div className="imageUpload">
+                        <input/>
+                        <input/>
+                        <input/>
+                    </div>
+
+
+                    <input type="submit" placeholder="Save"/>
+
+
+                </form>
+
+
+                </div>
+
+                <div className="right-side">
+                    <h2> Sidebar</h2>
+                </div>
             </div>
         );
     }
