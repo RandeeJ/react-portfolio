1f94c8f0dd29bfec1a77f29cad98efc3e502a3d2
Randee Johnson <randeejohnson@Randees-MacBook-Pro.local>
2021-06-07 17:53:04 -0400

    Fixing Multiple Key Prop Warnings in React

--- src/components/app.js
+++ src/components/app.js
@@ -84,7 +84,7 @@ export default class App extends Component {
 
   authorizePages() {
     return [
-      <Route path="/portfolio-manager" component={PortfolioManager} />
+      <Route key="portfolio-manager" path="/portfolio-manager" component={PortfolioManager} />
       ]
   }
 
