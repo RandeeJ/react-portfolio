d9320033fcd200c73bd04f6593faab784b85815c
Randee Johnson <randeejohnson@Randees-MacBook-Pro.local>
2021-07-26 13:42:13 -0400

    Refactoring the Infinite Scroll Feature to Fix Memory Leak

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -17,26 +17,23 @@ class Blog extends Component{
         }
 
         this.getBlogItems = this.getBlogItems.bind(this);
-        this.activateInfiniteScroll();
+        this.onScroll = this.onScroll.bind(this);
+        window.addEventListener("scroll", this.onScroll, false);
 
         };
 
 
-activateInfiniteScroll() {
-    window.onscroll = () => {
-
-if (this.state.isLoading ||
-    this.state.blogItems.length === this.state.totalCount){
-    return;
-}
-
-
-
-    if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
-    this.getBlogItems();    
-    }
-        };
-    }
+    onScroll() {
+        if (this.state.isLoading ||
+            this.state.blogItems.length === this.state.totalCount
+            ) {
+                return;
+            }
+        if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight
+            ) {
+                this.getBlogItems();    
+            }
+        }
         
 
 
@@ -62,6 +59,10 @@ if (this.state.isLoading ||
 
         componentDidMount() {
             this.getBlogItems();
+        }
+
+        componentWillUnmount() {
+            window.removeEventListener("scroll", this.onScroll, false);
         }
 
     render() {
