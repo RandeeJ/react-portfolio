41e6248f502c550e3e9516b975091fa9a35977b0
Randee Johnson <randeejohnson@Randees-MacBook-Pro.local>
2021-07-27 19:31:35 -0400

    Creating Blog Posts via the API

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -27,7 +27,7 @@ export default class BlogModal extends Component {
     }
 
 handleSuccessfulFormSubmission(blog){
-    console.log("blog from blog form");
+    console.log("blog from blog form", blog);
 }
 
     render () {
