8c06ef5d3cc88a49396e09a81e39940da806182f
Randee Johnson <randeejohnson@Randees-MacBook-Pro.local>
2021-08-15 11:39:14 -0400

    Adding the Ability to Upload Featured Images to Blogs

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import { DropzoneComponent } from 'react-dropzone-component';
 
 import RichTextEditor from "../forms/rich-text-editor";
-import { faWizardsOfTheCoast } from '@fortawesome/free-brands-svg-icons';
 
 export default class BlogForm extends Component {
     constructor(props){
@@ -22,6 +21,9 @@ export default class BlogForm extends Component {
         this.componentConfig = this.componentConfig.bind(this);
         this.djsConfig = this.djsConfig.bind(this);
         this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this);
+
+        this.featuredImageRef = React.createRef();
+
     }
 
 
@@ -42,8 +44,8 @@ export default class BlogForm extends Component {
 
     handleFeaturedImageDrop() {
         return {
-            addedFile: file => this.setState({featured_image: file})
-        }
+            addedfile: file => this.setState({featured_image: file})
+        };
     }
 
 handleRichTextEditorChange(content) {
@@ -56,7 +58,12 @@ handleRichTextEditorChange(content) {
 
         formData.append("portfolio_blog[title]", this.state.title);
         formData.append("portfolio_blog[blog_status]", this.state.blog_status);
-        formData.append("portfolio_blog[content]", this.state.content)
+        formData.append("portfolio_blog[content]", this.state.content);
+
+        if (this.state.featured_image) {
+            formData.append("portfolio_blog[featured_image]", this.state.featured_image);
+
+        }
 
         return formData;
 
@@ -65,13 +72,17 @@ handleRichTextEditorChange(content) {
 handleSubmit(event) {
     axios.post("https://randeejohnson.devcamp.space/portfolio/portfolio_blogs", this.buildForm(), {withCredentials: true}
     ). then(response => {
+        if (this.state.featured_image) {
+            this.featuredImageRef.current.dropzone.removeAllFiles()}
         
 
         this.setState({
             title: "",
             blog_status: "",
-            content: ""
+            content: "",
+            featured_image: ""
         })
+        
 
         this.props.handleSuccessfulFormSubmission
         (response.data.portfolio_blog
@@ -122,11 +133,12 @@ handleChange(event) {
 
                 <div className="image-uploaders">
                     <DropzoneComponent
+                    ref = {this.featuredImageRef}
                     config = {this.componentConfig()}
-                    djsConfig = {this.djsConfig}
-                    eventHandlers = { this.handleFeaturedImageDrop}>
+                    djsConfig = {this.djsConfig()}
+                    eventHandlers = { this.handleFeaturedImageDrop()}>
 
-                        <div className="dz-message">Featured image
+                        <div className="dz-message">Featured Image
                         </div>
 
                     </DropzoneComponent>
