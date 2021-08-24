95d25b2d008cc192d1bbc7abf777beab635ce0f0
Randee Johnson <randeejohnson@Randees-MacBook-Pro.local>
2021-08-24 11:45:28 -0400

    Building About Page Styles for React Portfolio

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import aboutUsImage from "../../../static/assets/images/auth/cheer-clipart-minnie-mouse-7.gif"
+
+
 
 export default function() {
+
     return (
-        <div>
-            About
+
+        
+        <div className="about-container">
+            <div className="about-image-container"
+            style={{
+                background: "url(" + aboutUsImage + ") no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "center"
+            }}/>
+
+
+            <div className="about-bio-container">
+                <div className="about-bio-content">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum commodi excepturi dolorem illo consequatur asperiores adipisci quas error libero inventore laboriosam, ullam voluptatum, nam ex sequi, vero unde non sunt! Modi suscipit tempore cumque maiores, nam nisi ab! Blanditiis laudantium eos deleniti assumenda nisi perferendis odio soluta! Aspernatur optio fuga veniam labore, voluptatum eaque, quasi id aut alias dolores nobis! Optio, totam, quod fuga amet, facere molestias natus ad tenetur sed ipsum sequi vero hic incidunt doloremque eveniet quasi harum quia ullam? Natus dolorum deleniti minus iste cum ab laboriosam. Deserunt consectetur veritatis in, doloremque eligendi laudantium dicta quo dignissimos error non qui est, reprehenderit commodi illum laboriosam velit consequatur incidunt omnis? Repellat sequi rem explicabo, possimus officia doloribus necessitatibus! Atque numquam aliquid alias voluptatum ipsum sint impedit placeat blanditiis, vitae facere eaque asperiores ducimus, architecto, error harum excepturi minima deleniti eligendi corporis facilis saepe? Facilis esse optio culpa nam. Exercitationem molestias repudiandae assumenda voluptas dignissimos officiis pariatur quod minus tenetur, sint labore, quaerat ea officia reiciendis ratione ad nemo aperiam velit eos hic asperiores soluta natus magnam? Vero, tempore! Ad, nobis, enim laboriosam corporis facilis laudantium blanditiis, quas molestias accusantium libero totam dolor minima impedit. Similique eveniet quibusdam expedita temporibus harum! Voluptatum voluptate sapiente illum in, inventore sunt aliquid! Nesciunt commodi ipsam doloribus nemo iure delectus sapiente dicta ipsa. Voluptatum, nam enim. Accusantium dolorum ipsam quae quas. Quos, praesentium. Error expedita numquam esse accusamus sequi aspernatur tenetur totam incidunt? Culpa accusantium odio porro sint officia labore inventore laborum deleniti ipsam in itaque, dolorem ut harum temporibus, doloremque eligendi? Sequi, quidem et? Quibusdam aliquam at, veniam suscipit harum nulla error? Quaerat tenetur nisi aliquam minima magnam dolorum recusandae non quibusdam dicta odio, obcaecati quisquam ad consequatur atque. 
+                </div>
+            </div>
         </div>
     );
 }
