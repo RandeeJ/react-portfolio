678e8cc76f2402ea44e670ae2e82b6decb1bfef3
Randee Johnson <randeejohnson@Randees-MacBook-Pro.local>
2021-08-29 21:48:44 -0400

    Full Filtering Feature Build Out in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -22,30 +22,35 @@ this.handleFilter = this.handleFilter.bind(this);
 }
 
 handleFilter(filter) {
-    this.setState({
-        data: this.state.data.filter(item => {
-            return item.category === filter;
-        })
-    });
-}
+    if (filter === "CLEAR_FILTERS") {
+        this.getPortfolioItems();
+    }
+    else
+    {
+        this.getPortfolioItems(filter);
+        }}
+   
 
-getPortfolioItems() {
+getPortfolioItems(filter = null) {
     axios.get("https://randeejohnson.devcamp.space/portfolio/portfolio_items")
   .then(response => {
-    // handle success
-    this.setState({
-        data: response.data.portfolio_items
+      if (filter) {
+          this.setState({
+        data: response.data.portfolio_items.filter(item => {
+            return item.category === filter;})
+      })
+    }
+    else{
+        this.setState({
+            data: response.data.portfolio_items
     })
-  })
+  }
+})
   .catch(error => {
     // handle error
     console.log(error);
-  })
-  // NEED TO FIGURE THIS OUT
-  .then( () => {
-    // always executed
   });
-  }
+}
 
 
     portfolioItems() {  
@@ -80,16 +85,24 @@ componentDidMount() {
 
 
         return (
-            <div className="portfolio-items-wrapper">
+            <div className="homepage-wrapper">
+                <div className = "filter-links">
                 <button className="btn" onClick = {() => this.handleFilter("eCommerce")}>eCommerce
                 </button>
                 <button className="btn" onClick = {() => this.handleFilter("Scheduling")}>Scheduling
                 </button>
                 <button className="btn" onClick = {() => this.handleFilter("Enterprise")}>Enterprise
                 </button>
+                <button className="btn" onClick = {() => this.handleFilter("CLEAR_FILTERS")}>All
+                </button>
+                </div>
 
-                {this.portfolioItems()}</div>
+                <div className="portfolio-items-wrapper">
 
+                {this.portfolioItems()}
+                
+                </div>
+                </div>
 
                 /* <button onClick={this.handlePageTitleUpdate}> Change Title </button> */
            
