a99b4202d052db86cda743ef845ec7f68545d3a0
Randee Johnson <randeejohnson@Randees-MacBook-Pro.local>
2021-08-31 18:13:40 -0400

    Heroku Deployment

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
 export default class Login extends Component {
   constructor(props) {
     super(props);
@@ -10,7 +9,7 @@ export default class Login extends Component {
     this.state = {
       email: "",
       password: "",
-      errorText: ""
+      errorText: "",
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -20,7 +19,7 @@ export default class Login extends Component {
   handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value,
-      errorText: ""
+      errorText: "",
     });
   }
 
@@ -31,27 +30,27 @@ export default class Login extends Component {
         {
           client: {
             email: this.state.email,
-            password: this.state.password
-          }
+            password: this.state.password,
+          },
         },
         { withCredentials: true }
       )
-      .then(response => {
-        if(response.data.status === "created") {
-            // console.log("you can enter!")
-            this.props.handleSuccessfulAuth();
+      .then((response) => {
+        if (response.data.status === "created") {
+          // console.log("you can enter!")
+          this.props.handleSuccessfulAuth();
         } else {
-            this.setState({
-                errorText: "Wrong email or password"
-            });
-            this.props.handleUnsuccessfulAuth();
-        }      
-      }).catch(error => {
           this.setState({
-              errorText: "An error occurred"
+            errorText: "Wrong email or password",
           });
           this.props.handleUnsuccessfulAuth();
-
+        }
+      })
+      .catch((error) => {
+        this.setState({
+          errorText: "An error occurred",
+        });
+        this.props.handleUnsuccessfulAuth();
       });
 
     event.preventDefault();
@@ -64,31 +63,31 @@ export default class Login extends Component {
 
         <div> {this.state.errorText}</div>
 
-        <form onSubmit={this.handleSubmit}  className="auth-form-wrapper">
+        <form onSubmit={this.handleSubmit} className="auth-form-wrapper">
           <div className="form-group">
-            <FontAwesomeIcon icon="envelope"/>
-          <input
-            type="email"
-            name="email"
-            placeholder="Your email"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className ="form-group">
-          <FontAwesomeIcon icon="lock" />
-          <input
-            type="password"
-            name="password"
-            placeholder="Your password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
+            <FontAwesomeIcon icon="envelope" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <FontAwesomeIcon icon="lock" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Your password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
           </div>
 
-          
-            <button className="btn" type="submit">Login</button>
-          
+          <button className="btn" type="submit">
+            Login
+          </button>
         </form>
       </div>
     );
